e01835948ac9e87a167c3f8442ed662310eb4606
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-16 21:26:19 +0530

    Zod

--- Auth/Final.js
+++ Auth/Final.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+
 function checkUserId(username){
     if(!username){
         
     }
 }
 app.post('/signup',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
 })
 app.post('/signin',function(req,res){
 
