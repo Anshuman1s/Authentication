a66e01b3f05f220d2eb6e57b3bec67ba95aeba25
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-20 00:14:32 +0530

    Middleware_checks

--- Middleware/admin.js
+++ Middleware/admin.js
@@ -1,6 +1,19 @@
 // Middleware For handling auth
-
+const {Admin} = require("../MongoDb")
 function adminMiddleware(req,res,next){
-    
+    const username = req.header.username;
+    const password = req.header.password;
+    Admin.findOne({
+        username:username,
+        password:password
+    }).then(function(value){
+        if(value){
+            next()
+        }else{
+            res.status(403).json({
+                msg:"Admin Doesn't Exist"
+            })
+        }
+    })
 }
 module.exports = adminMiddleware;
