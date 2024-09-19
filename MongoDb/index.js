d08fb7cd5c081189355e3e10c60a6010690b3945
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-19 23:18:47 +0530

    MongoDB

---
+++ MongoDb/index.js
@@ -0,0 +1,13 @@
+// You need to implement a course selling app. Make sure you setup youer own mongodb
+//  instance before starting
+//mongodb+srv:anshumanshukla9918:N3JbxtA9v8aK0TlL@cluster0.hqksj.mongodb.net/
+
+const mongoose = require('mongoose');
+// Connting to MongoDB
+mongoose.connect('url')
+
+// Define Schema
+const AdminSchema = new mongoose.Schema({
+    // schema definition here
+})
+
