f8d97ade4a1728171ef33b0d747cac1bad98dac1
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-20 00:01:55 +0530

    Schema

--- MongoDb/index.js
+++ MongoDb/index.js
@@ -4,19 +4,29 @@
 
 const mongoose = require('mongoose');
 // Connting to MongoDB
-mongoose.connect('url')
+mongoose.connect('mongodb+srv://anshumanshukla9918:N3JbxtA9v8aK0TlL@cluster0.hqksj.mongodb.net/')
 
 // Define Schema
 const AdminSchema = new mongoose.Schema({
     // schema definition here
+    username:String,
+    password:String
 })
 
 const UserSchema = new mongoose.Schema({
-
+    username:String,
+    password:String,
+    purchasedCourses:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course'
+    }]
 })
 
 const CourseSchema = new mongoose.Schema({
-
+    title:String,
+    description:String,
+    imageLink:String,
+    price:Number
 })
 
 const Admin = mongoose.model('Admin',AdminSchema);
