e2a16ca0286d7fad4f5e9f6d3aacc5ce608aecbd
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-22 10:03:29 +0530

    Index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const adminRouter = require("./Routes/Admin");
-const userRouter = require('./Routes/User');
-\;
+const app = express();
+const adminRouter = require("./Routes/Admin")
+const userRouter = require("./Routes/User");
+
+// Middleware for parsing request bodies
 app.use(bodyParser.json());
-app.use("/Admin",adminRouter);
-app.use("/User",userRouter);
+app.use("/Admin", adminRouter)
+app.use("/User", userRouter)
 
 const PORT = 3000;
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on Port ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
