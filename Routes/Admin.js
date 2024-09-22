2a45f83aca0f1fd50afa8632151c3fddcd5d4fc1
Anshuman <anshumanshukla.12as@gmail.com>
2024-09-22 10:02:32 +0530

    Routes-Course-Selling

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -1,15 +1,45 @@
-const {Router} = require('express');
-const adminMiddleware = require('../Middleware/admin')
+const { Router } = require("express");
+const adminMiddleware = require("../Middleware/admin");
+const { Admin } = require("../MongoDb");
 const router = Router();
 
-router.post('/signup',function(req,res){
+// Admin Routes
+router.post('/signup',async (req, res) => {
+    // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
 
-})
-router.post('/courses',adminMiddleware,(req,res)=>{
+    await Admin.create({
+        username:username,
+        password:password
+    })
+    // .then(function(){
+    //     res.send({
+    //         message:"Admin Created Successfully"
+    //     })
+    // }).catch(function(){
+    //     res.json({
+    //         messgage:"User not Created"
+    //     })
+    // })
+});
 
-})
-router.get('/courses', function(req,res){
+router.post('/signin', (req, res) => {
+    // Implement admin signup logic
+});
 
-})
+router.post('/courses', adminMiddleware, (req, res) => {
+    // Implement course creation logic
+});
+
+router.get('/courses', adminMiddleware, (req, res) => {
+    // Implement fetching all courses logic
+    if(!course){
+        res.status(401).send({
+        "Messgae":"You have not Buy any course";
+        
+        })
+    }
+});
 
 module.exports = router;
