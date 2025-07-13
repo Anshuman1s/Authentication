cba82bf6eb5f96f8f41d7af9a97575248b7036a2
Anshuman shukla <117532540+Anshuman1s@users.noreply.github.com>
2025-07-13 23:06:13 +0530

    Update Admin.js

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -8,7 +8,6 @@ router.post('/signup',async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
-
     await Admin.create({
         username:username,
         password:password
