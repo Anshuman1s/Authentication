c0db4f9ba14cf181e443e94ba56b0db4666b9f7e
Anshuman shukla <117532540+Anshuman1s@users.noreply.github.com>
2025-08-17 17:15:40 +0530

    Update User.js

--- Middleware/User.js
+++ Middleware/User.js
@@ -1,6 +1,6 @@
 const {User} = require('../MongoDb')
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
+    // Implement user auth logic.
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
     const username = req.header.username;
     const password = req.header.password;
