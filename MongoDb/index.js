fa9e4cd45570f15ae6534643a99f558291f61aed
Anshuman shukla <117532540+Anshuman1s@users.noreply.github.com>
2025-09-05 12:56:20 +0530

    Update index.js

--- MongoDb/index.js
+++ MongoDb/index.js
@@ -29,7 +29,6 @@ const CourseSchema = new mongoose.Schema({
 const Admin = mongoose.model('Admin',AdminSchema);
 const User = mongoose.model('User',UserSchema);
 const Course = mongoose.model('Course',CourseSchema)
-
 module.exports={
     Admin,
     User,
